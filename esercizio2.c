22f618f5b259461b090a2b13ce239d281f7e4457
FrancescoPioPapparella <109146578+FrancescoPioPapparella@users.noreply.github.com>
2022-07-14 14:45:45 +0200

    Add files via upload

---
+++ esercizio2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+main () {
+int a, b ;   /*numeri inseriti*/
+float somma  ;   /*somma dei due numeri*/
+float media ;    /*media dei numeri*/
+
+scanf("%d", &a) ; /*lettura dei due  numeri*/
+scanf("d",  &b) ; /*lettura dei due numeri*/
+
+somma = a+b ;  /*con questo comando si calcola la somma dei due numeri*/
+media = somma / 2 ;  /*con questo comando si calcola la media dei due numeri*/
+printf("d", media) ;  /*stampa il risultato*/
+}
